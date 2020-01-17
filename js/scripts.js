278d3d4701965b2c17665c1028abdf09fd27063a
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-17 10:36:19 -0800

    create hide/fadeIn function for each question so you can click from start to question 5

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,24 @@ $(document).ready(function() {
     $("#q1").fadeIn();
   })
 
+  $("#btn1").click(function(){
+    $("#q1").hide();
+    $("#q2").fadeIn();
+  });
+
+  $("#btn2").click(function(){
+    $("#q2").hide();
+    $("#q3").fadeIn();
+  });
+
+  $("#btn3").click(function(){
+    $("#q3").hide();
+    $("#q4").fadeIn();
+  });
+
+  $("#btn4").click(function(){
+    $("#q4").hide();
+    $("#q5").fadeIn();
+  });
+
 });
